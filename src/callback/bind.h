bad67e7b6cd663e433c2ff24d2bbb14591083c3f
Chuanjie <chinasasu@gmail.com>
2016-10-24 15:25:07 +0800

    代码整理规范

--- src/callback/bind.h
+++ src/callback/bind.h
@@ -77,248 +77,42 @@ c. 弱指针
 //	return Callback<void(void)>(new BindStorage<false,void, void, void>(function, args...));
 //}
 
+template <bool IsWeakCall, typename F, typename Runner, typename... UnboundArgs>
+struct CallbackTraits;
 
-template <bool IsMethod, typename... Args>
-struct IsWeakMethodEx : public std::false_type {};
-
-
-template <typename T, typename... Args>
-struct IsWeakMethodEx<true, std::weak_ptr<T>, Args...> : public std::true_type {};
-
-template <typename T, typename... Args>
-struct IsWeakMethodEx<true, std::weak_ptr<T>&, Args...> : public std::true_type {};
-
-template <typename T, typename... Args>
-struct IsWeakMethodEx<true, const std::weak_ptr<T>&, Args...> : public std::true_type {};
-
-
-template <bool IsWeakCall, typename... Args>
-struct RunnerType
+template <bool IsWeakCall, typename F, typename Runner, typename... UnboundArgs>
+struct CallbackTraits<IsWeakCall, F, Runner, TypeList<UnboundArgs...>>
 {
-    using type = void;
-};
+	using ReturnType = typename function_traits<F>::return_type;
 
-template <typename T, typename... Args>
-struct RunnerType<true, T, Args...>
-{
-    using type = T;
-};
+	using Type = Callback<ReturnType(UnboundArgs...)>;
 
-
-template <bool IsMethod, typename... Args>
-struct BoundArgsNum;
-
-template<typename T, typename... Args>
-struct BoundArgsNum<true, T, Args...>
-{
-    static constexpr std::size_t size = sizeof...(Args);
-};
-
-
-template<typename... Args>
-struct BoundArgsNum<false, Args...>
-{
-    static constexpr std::size_t size = sizeof...(Args);
-};
-
-
-/*
- 1. 分离绑定参数和运行时参数
- 2. 生成std::function变量存储
- 3. Run时绑定运行时参数
-*/
-
-// https://functionalcpp.wordpress.com/2013/08/05/function-traits/
-
-template<class F>
-struct function_traits;
-
-// function pointer
-template<class R, class... Args>
-struct function_traits<R(*)(Args...)> : public function_traits<R(Args...)>
-{};
-
-template<class R, class... Args>
-struct function_traits<R(Args...)>
-{
-    using return_type = R;
-    
-    static constexpr std::size_t arity = sizeof...(Args);
-    
-    template <std::size_t N>
-    struct argument
-    {
-        static_assert(N < arity, "error: invalid parameter index.");
-        using type = typename std::tuple_element<N,std::tuple<Args...>>::type;
-    };
-    
-    using tuple_type = std::tuple<Args...>;
-};
-
-// member function pointer
-template<class C, class R, class... Args>
-struct function_traits<R(C::*)(Args...)> : public function_traits<R(C&,Args...)>
-{};
-
-template<class C, class R, class T, class... Args>
-struct function_traits<R(C::*)(std::weak_ptr<T>,Args...)> : public function_traits<R(C&,T*, Args...)>
-{};
-
-// const member function pointer
-template<class C, class R, class... Args>
-struct function_traits<R(C::*)(Args...) const> : public function_traits<R(C&,Args...)>
-{};
-
-// member object pointer
-template<class C, class R>
-struct function_traits<R(C::*)> : public function_traits<R(C&)>
-{};
-
-
-template <typename...>
-struct types {};
-
-template <typename T, typename... U>
-struct types_link
-{
-    using type = types<T, U...>;
-};
-
-template <typename T, typename... U>
-struct types_link<T, types<U...>>
-{
-    using type = types<T, U...>;
-};
-
-template <typename... T, typename U>
-struct types_link<types<T...>, U>
-{
-    using type = types<T..., U>;
-};
-
-template<int RestI, typename ArgsTuple, typename... P>
-struct UnboundArgType
-{
-    static constexpr std::size_t size = std::tuple_size<ArgsTuple>::value;
-    
-    using type_this = typename std::tuple_element<size - RestI, ArgsTuple>::type;
-    using type_list = typename types_link<P...,type_this>::type;
-    
-    using type = typename UnboundArgType<RestI - 1, ArgsTuple, type_list>::type;
-};
-
-
-template<typename ArgsTuple, typename... P>
-struct UnboundArgType<0,ArgsTuple, P...>
-{
-    
-    using type = typename std::conditional<(sizeof...(P) == 0), types<>, types<P...>>::type ;
-};
-
-template<typename ArgsTuple, typename... P>
-struct UnboundArgType<0,ArgsTuple, types<P...>> : public UnboundArgType<0,ArgsTuple, P...>{};
-
-
-template<int N, typename T, typename U, typename... P>
-struct unbound_type
-{
-private:
-    
-    using t_type = typename std::tuple_element<N-1,T>::type;
-    using u_type = typename std::tuple_element<N-1,U>::type;
-    
-    using maybe_type = typename types_link<t_type,P...>::type;
-    
-    using type_this = typename std::conditional<(std::is_placeholder<u_type>::value > 0), maybe_type, types<P...>>::type ;
-
-public:
-    using type = typename unbound_type<N-1, T, U, type_this>::type;
-    
-};
-
-
-template<int N, typename T, typename U, typename... P>
-struct unbound_type<N,T,U,types<P...>> : unbound_type<N, T, U, P...>{};
-
-template<typename T, typename U, typename... P>
-struct unbound_type<0, T, U, P...>
-{
-    using type = types<P...>;
-};
-
-
-template<typename T, typename U, typename... P>
-struct unbound_type<0, T, U, types<P...>> : unbound_type<0, T, U, P...> {};
-
-
-template <bool IsWeakCall, typename F,typename Runner, typename... UnboundArgs>
-struct make_callback_ex;
-
-template <bool IsWeakCall, typename F, typename Runner,typename... UnboundArgs>
-struct make_callback_ex<IsWeakCall, F, Runner, types<UnboundArgs...>>
-{
-    
-    using return_type = typename function_traits<F>::return_type;
-
-    using type = Callback<return_type(UnboundArgs...)>;
-    
-    using storage_type = BindStorage<IsWeakCall, return_type, Runner, UnboundArgs... >;
-};
-
-
-
-template <typename R, typename... UnboundArgs>
-struct make_callback;
-
-
-template <typename R, typename... UnboundArgs>
-struct make_callback<R, types<UnboundArgs...>>
-{
-    using type = Callback<R(UnboundArgs...)>;
-    
-    using storage_type = BindStorage<false, R, void, UnboundArgs... >;
-};
-
-template <typename R>
-struct make_callback<R, types<void>>
-{
-    using type = Callback<R(void)>;
-    
-    using storage_type = BindStorage<false, R, void >;
-};
-
-
-template<typename First, typename... Rest>
-struct UnboundArgsNum
-{
-    enum { value = UnboundArgsNum<First>::value + UnboundArgsNum<Rest...>::value };
-};
-
-template<typename Last>
-struct UnboundArgsNum<Last>
-{
-    enum{ value = (std::is_placeholder<Last>::value > 0 ? 1 : 0) };
+	using StorageType = BindStorage<IsWeakCall, ReturnType, Runner, UnboundArgs... >;
 };
 
 template<typename F, typename... Args>
 decltype(auto) Bind(F&& function, const Args&... args)
 {
-    using FuncArgsTuple = typename function_traits<F>::tuple_type;
+	// bind的参数个数，如果是成员函数，不计算第一个参数
+    using BoundArgsNumType = BoundArgsSize<std::is_member_function_pointer<F>::value, Args...> ;
     
-    using BoundArgsNumType = BoundArgsNum<std::is_member_function_pointer<F>::value, Args...> ;
+    // 未绑定的参数个数 = 函数参数个数 - 绑定的参数个数 - 成员函数指针本身
+	static constexpr std::size_t UnBoundTypeSize = 
+		function_traits<F>::arity - BoundArgsNumType::size - (std::is_member_function_pointer<F>::value ? 1 : 0);
+
+	// 未绑定的参数类型
+    using UnboundTypeList = typename UnboundTypeTraits<UnBoundTypeSize,	typename function_traits<F>::tuple_type>::type;
     
-    // if method set 1
-    static constexpr std::size_t MethodRunnerArg = std::is_member_function_pointer<F>::value ? 1 : 0;
+    using IsWeakCall = IsWeakMethod<std::is_member_function_pointer<F>::value, Args...>;
+    using MethodRunner = typename MethodRunnerTraits<std::is_member_function_pointer<F>::value, Args...>::type;
     
-    using Unbound = typename UnboundArgType<function_traits<F>::arity - BoundArgsNumType::size - MethodRunnerArg,FuncArgsTuple>::type;
+	// callback类型
+    using CallbackType = typename CallbackTraits<IsWeakCall::value, F, MethodRunner, UnboundTypeList>::Type;
+	
+	// 存储类型
+    using StorageType = typename CallbackTraits<IsWeakCall::value, F, MethodRunner, UnboundTypeList>::StorageType;
     
-    using IsWeakCall = IsWeakMethodEx<std::is_member_function_pointer<F>::value, Args...>;
-    using MethodRunner = typename RunnerType<std::is_member_function_pointer<F>::value, Args...>::type;
-    
-    using callback_type = typename make_callback_ex<IsWeakCall::value, F, MethodRunner,Unbound>::type;
-    using st_type = typename make_callback_ex<IsWeakCall::value, F,MethodRunner,Unbound>::storage_type;
-    
-    return callback_type(new st_type(function, args...));
+    return CallbackType(new StorageType(function, args...));
 }
 
 typedef Callback<void(void)> Closure;
--- src/callback/bind_helpers.h
+++ src/callback/bind_helpers.h
@@ -14,12 +14,141 @@
 
 class BindStorageBase;
 
-template <typename T>
+template <bool IsMethod, typename... Args>
 struct IsWeakMethod : public std::false_type {};
 
-template <typename T>
-struct IsWeakMethod<std::weak_ptr<T> > : public std::true_type {};
+template <typename T, typename... Args>
+struct IsWeakMethod<true, std::weak_ptr<T>, Args...> : public std::true_type {};
 
+template <typename T, typename... Args>
+struct IsWeakMethod<true, std::weak_ptr<T>&, Args...> : public std::true_type {};
+
+template <typename T, typename... Args>
+struct IsWeakMethod<true, const std::weak_ptr<T>&, Args...> : public std::true_type {};
+
+template <bool IsMethod, typename... Args>
+struct MethodRunnerTraits
+{
+	using type = void;
+};
+
+template <typename T, typename... Args>
+struct MethodRunnerTraits<true, T, Args...>
+{
+	using type = T;
+};
+
+
+template <bool IsMethod, typename... Args>
+struct BoundArgsSize;
+
+template<typename T, typename... Args>
+struct BoundArgsSize<true, T, Args...>
+{
+	static constexpr std::size_t size = sizeof...(Args);
+};
+
+template<typename... Args>
+struct BoundArgsSize<false, Args...>
+{
+	static constexpr std::size_t size = sizeof...(Args);
+};
+
+
+/*
+1. 分离绑定参数和运行时参数
+2. 生成std::function变量存储
+3. Run时绑定运行时参数
+*/
+
+// https://functionalcpp.wordpress.com/2013/08/05/function-traits/
+
+template<class F>
+struct function_traits;
+
+// function pointer
+template<class R, class... Args>
+struct function_traits<R(*)(Args...)> : public function_traits<R(Args...)>
+{};
+
+template<class R, class... Args>
+struct function_traits<R(Args...)>
+{
+	using return_type = R;
+
+	static constexpr std::size_t arity = sizeof...(Args);
+
+	template <std::size_t N>
+	struct argument
+	{
+		static_assert(N < arity, "error: invalid parameter index.");
+		using type = typename std::tuple_element<N, std::tuple<Args...>>::type;
+	};
+
+	using tuple_type = std::tuple<Args...>;
+};
+
+// member function pointer
+template<class C, class R, class... Args>
+struct function_traits<R(C::*)(Args...)> : public function_traits<R(C&, Args...)>
+{};
+
+// weakptr member function pointer
+template<class C, class R, class T, class... Args>
+struct function_traits<R(C::*)(std::weak_ptr<T>, Args...)> : public function_traits<R(C&, T*, Args...)>
+{};
+
+// const member function pointer
+template<class C, class R, class... Args>
+struct function_traits<R(C::*)(Args...) const> : public function_traits<R(C&, Args...)>
+{};
+
+// member object pointer
+template<class C, class R>
+struct function_traits<R(C::*)> : public function_traits<R(C&)>
+{};
+
+
+template <typename...>
+struct TypeList {};
+
+template <typename T, typename... U>
+struct TypesLink
+{
+	using type = TypeList<T, U...>;
+};
+
+template <typename T, typename... U>
+struct TypesLink<T, TypeList<U...>>
+{
+	using type = TypeList<T, U...>;
+};
+
+template <typename... T, typename U>
+struct TypesLink<TypeList<T...>, U>
+{
+	using type = TypeList<T..., U>;
+};
+
+template<int RestI, typename ArgsTuple, typename... P>
+struct UnboundTypeTraits
+{
+	static constexpr std::size_t ArgSize = std::tuple_size<ArgsTuple>::value;
+
+	using type_this = typename std::tuple_element<ArgSize - RestI, ArgsTuple>::type;
+	using type_list = typename TypesLink<P..., type_this>::type;
+
+	using type = typename UnboundTypeTraits<RestI - 1, ArgsTuple, type_list>::type;
+};
+
+template<typename ArgsTuple, typename... P>
+struct UnboundTypeTraits<0, ArgsTuple, P...>
+{
+	using type = typename std::conditional<(sizeof...(P) == 0), TypeList<>, TypeList<P...>>::type;
+};
+
+template<typename ArgsTuple, typename... P>
+struct UnboundTypeTraits<0, ArgsTuple, TypeList<P...>> : public UnboundTypeTraits<0, ArgsTuple, P...> {};
 
 template <bool IsWeakCall, typename Storage, typename R, typename... Args>
 struct InvokerHelper;
